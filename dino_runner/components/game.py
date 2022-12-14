59df3ad2f4b92fd1570815308ad690bd90f9762d
luissandon <Luis.Sandon@fundacion-jala.org>
2022-12-14 11:55:44 -0500

    Dinno runner with implementation of birds
    Luis Fernando Sandon

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,9 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
+from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
 
 class Game:
     def __init__(self):
@@ -14,6 +17,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,14 +35,18 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
-        pygame.display.flip()
+        # pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width()
